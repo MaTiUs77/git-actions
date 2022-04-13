0f15fda335b7f9e01b213d404697ce3277afce7a
Matias Flores <matius77@gmail.com>
2022-04-13 15:25:22 -0300

    Deploy with SHA

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Github Actions done!')
+  res.send(`Github Actions`)
 })
 
 app.listen(process.env.PORT, () => {
